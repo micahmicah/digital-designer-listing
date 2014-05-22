bdf4b43cc063b282e069e232b78899380e69c70c
Micah Barrett <mbarrett02@risd.edu>
2014-05-22 12:14:16 -0400

    initial upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+
+	   	$(window).scroll( function (){
+			if ($(window).scrollTop() > 900){
+
+			$('.intro').css('opacity', '0');
+			$('h1').css('color','orange');
+		
+
+			} else {
+
+			$('.intro').css('opacity', '1');
+			$('h1').css('color','blue')
+			
+			}
+
+		})
+
+
+		$(window).scroll( function (){
+			if ($(window).scrollTop() > 1200){
+
+			
+			$('h3').replaceWith('<h3>Responsibilities</h3>');
+		
+
+			} else {
+
+			$('h3').replaceWith('<h3>info</h3>');
+			
+			
+			}
+
+		})
+    
+	
