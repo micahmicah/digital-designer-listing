c92d0bb8c5a74af8f927cc73d44a951b7338b9fd
Micah Barrett <mbarrett02@risd.edu>
2014-05-22 18:40:25 -0400

    color adjustments

--- js/main.js
+++ js/main.js
@@ -33,8 +33,10 @@
 			apply_bottom = $('.apply')[0].getBoundingClientRect().bottom;
 			if(apply_bottom < window.innerHeight ){
 				$('.apply').css('background','red').css('background','blue').css('color', 'white');
+				$('h3').replaceWith('<h3>Join Us</h3>');
 			} else{
 				$('.apply').css('background', 'none').css('color','blue');
+				$('h3').replaceWith('<h3>Info</h3>');
 			}
 		}
 
@@ -43,20 +45,20 @@
 
 
 
-		$(window).scroll( function (){
-			if ($(window).scrollTop() > 1200){
+		// $(window).scroll( function (){
+		// 	if ($(window).scrollTop() > 1200){
 
 			
-			$('h3').replaceWith('<h3>Responsibilities</h3>');
+		// 	$('h3').replaceWith('<h3>Responsibilities</h3>');
 		
 
-			} else {
+		// 	} else {
 
-			$('h3').replaceWith('<h3>info</h3>');
+		// 	$('h3').replaceWith('<h3>info</h3>');
 			
 			
-			}
+		// 	}
 
-		})
+		// })
     
 	
